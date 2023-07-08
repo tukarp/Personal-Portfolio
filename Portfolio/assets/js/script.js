a08cd91e60ce314662df74a9bb3d17e2d7cf9481
tukarp <114019481+tukarp@users.noreply.github.com>
2023-07-08 19:20:13 +0200

    Update home page

--- Portfolio/assets/js/script.js
+++ Portfolio/assets/js/script.js
@@ -1,5 +1,6 @@
 let navLinks = document.querySelectorAll("nav ul li a");
 
+// Sidebar active section changer function
 navLinks.forEach((item) => {
     item.addEventListener("click", function() {
         document.querySelector("nav ul li a.active").classList.remove("active");
@@ -7,4 +8,12 @@ navLinks.forEach((item) => {
         document.querySelector("main > section.active").classList.remove("active");
         document.querySelector(`main > section${item.getAttribute('href')}`).classList.add("active");
     });
+});
+
+
+
+// Sidebar menu toggle function for mobile
+document.querySelector("#sidebar .toggle-sidebar").click().addEventListener("click", function() {
+    document.querySelector("#sidebar").classList.toggle("open");
+
 });
