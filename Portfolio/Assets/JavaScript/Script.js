54738a64950f090039b65b0d842a577a95e31d01
tukarp <114019481+tukarp@users.noreply.github.com>
2023-09-07 22:44:25 +0200

    Minor changes and fixes

--- Portfolio/Assets/JavaScript/Script.js
+++ Portfolio/Assets/JavaScript/Script.js
@@ -23,8 +23,10 @@ document.querySelector("#sidebar .toggle-sidebar").addEventListener("click", fun
 // Typed.js for the Home section header
 new Typed(".field h2", {                                                                                        // Creating a new Typed.js instance for the Home section field h2 header
     strings:                                                                                                    // Defining the strings to be typed
-        ["Junior Software Tester","Aspiring Back-End Developer",
-        "Aspiring Full Stack Developer", "Student at UMCS, Lublin"],
+        ["Junior Software Tester",
+         "Aspiring Software Engineer",
+          "Aspiring Full Stack Developer",
+          "Student at UMCS, Lublin"],
     typeSpeed: 80,                                                                                              // Defining typing speed
     backSpeed: 50,                                                                                              // Defining backspacing speed
     loop: true,                                                                                                 // Defining looping the strings
